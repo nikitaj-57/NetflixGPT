99388a836b8b932064459e28b0242e6b3630e0e1
nikitaj-57 <nikitajit2@gmail.com>
2023-09-20 12:04:21 +0530

    CRA and Tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">NetflixGPT</div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
